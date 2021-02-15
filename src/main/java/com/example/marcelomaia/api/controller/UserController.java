b21161fa2d1ef9ab6dda5a34f3eac703ff48df38
Marcelo Maia Juvencio <mj_maia@yahoo.com.br>
2021-02-15 11:54:59 -0300

    adicionando CORS.

--- src/main/java/com/example/marcelomaia/api/controller/UserController.java
+++ src/main/java/com/example/marcelomaia/api/controller/UserController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import com.example.marcelomaia.api.service.UserService;
 
 @RestController
 @RequestMapping("/users")
+@CrossOrigin(maxAge = 10, origins = {"http://localhost:4200"})
 public class UserController {
 	
 	@Autowired
